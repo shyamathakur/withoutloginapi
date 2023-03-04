3ad4b5ad3be0d7ff4c5f756f29ce49a5f81735d1
shyama <shyamathakur406@gmail.com>
2023-03-04 12:01:06 +0530

    message

---
+++ Middleware.ts
@@ -0,0 +1,15 @@
+// import { NextResponse } from "next/server";
+
+// const middleware = () => {
+//   const isCustomerPage = pageName?.startsWith("/[customerSlug]");
+
+//    if (!isCustomerPage) {
+//      return NextResponse.next();
+//   }
+  
+
+
+//   return NextResponse.next();
+// };
+
+// export default middleware;
